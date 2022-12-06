ac71a01d2be55dbefc921c5df3764746cc812e57
seunggeunYoo <yooyoosg@naver.com>
2022-12-06 14:34:43 +0900

    ..

--- 20.입출력[IO]/src/file/FileMain.java
+++ 20.입출력[IO]/src/file/FileMain.java
@@ -7,17 +7,18 @@ public class FileMain {
 	public static void main(String[] args) {
 		/*
 		 * 현재파일경로[기준경로]
-		 * 		eclise->C:\2022-11-JAVA-DEVELOPER\workspaceSE\20.입출력[IO]
+		 * 		Eclipse->C:\2022-11-JAVA-DEVELOPER\workspaceSE\20.입출력[IO]
 		 * 
 		 * 상대경로(relative)경로
 		 * 		현재경로를 기준으로 경로를 기술
-		 * 
-		 * 
+		 * 	*sample/a.txt
+		 *  .\sample\.atxt
 		 * 절대경로(absolute)경로
 		 * 		C:,D:드라이브를 기준으로 경로기술
 		 * 
 		 */
 		System.out.println(File.separatorChar);
+		File file1 = new File("데미안.txt");
 
 	}
 
