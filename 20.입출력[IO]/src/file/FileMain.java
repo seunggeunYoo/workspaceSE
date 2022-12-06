629bd3fa7f45d4cb2373c4eb75f3174c494cd97e
seunggeunYoo <yooyoosg@naver.com>
2022-12-06 15:27:10 +0900

    ..

--- 20.입출력[IO]/src/file/FileMain.java
+++ 20.입출력[IO]/src/file/FileMain.java
@@ -19,6 +19,96 @@ public class FileMain {
 		 */
 		System.out.println(File.separatorChar);
 		File file1 = new File("데미안.txt");
+		File file2 = new File("sample\\car1.jpg");
+		File file3 = new File("sample/subSample/00.CariableDeclare.txt");
+		File file4 = new File("C:\\2022-11-JAVA-DEVELOPER\\workspaceSE\\20.입출력[IO]\\sample\\");
+		
+		System.out.println("----------1.file path[relative]---------------");
+		System.out.println(file1.getPath());
+		System.out.println(file2.getPath());
+		System.out.println(file3.getPath());
+		System.out.println(file4.getPath());
+		System.out.println("----------2.file absoulute path---------------");
+		System.out.println(file1.getAbsolutePath());
+		System.out.println(file2.getAbsolutePath());
+		System.out.println(file3.getAbsolutePath());
+		System.out.println(file4.getAbsolutePath());
+		System.out.println("----------3.file name---------------");
+		System.out.println(file1.getName());
+		System.out.println(file2.getName());
+		System.out.println(file3.getName());
+		System.out.println(file4.getName());
+		System.out.println("----------4.file parent---------------");
+		System.out.println(file1.getParent());
+		System.out.println(file2.getParent());
+		System.out.println(file3.getParent());
+		System.out.println(file4.getParent());
+		
+		
+		File dir1 = new File ("sample");
+		File dir2 = new File ("sample/subSample1");
+		File dir3 = new File ("sample","subSample2");
+		System.out.println("--------5.IsDirectory, isFile-----------");
+		System.out.println(dir1.isDirectory());
+		System.out.println(dir2.isDirectory());
+		System.out.println(dir3.isDirectory());
+		System.out.println(dir1.isFile());
+		System.out.println(dir2.isFile());
+		System.out.println(dir3.isFile());
+		System.out.println(file1.isDirectory());
+		System.out.println(file2.isDirectory());
+		System.out.println(file3.isDirectory());
+		System.out.println(file4.isDirectory());
+		System.out.println("-----------6.디렉토리안에 파일(디렉토리)목록(String[])----------");
+		String[] fileNameList = dir1.list();
+		for(String fileName : fileNameList) {
+			System.out.println(fileName);
+		}
+		System.out.println("-----------7.디렉토리안에 파일(디렉토리)목록(File[])----------");
+		File[] fileList=dir1.listFiles();
+		for (File file : fileList) {
+			if(file.isDirectory()) {
+				//Directory
+				System.out.println("\tD:"+file.getName());
+				File[] subFileList=file.listFiles();
+				for (File subFile : subFileList) {
+					System.out.println("\t\t"+subFile.getName());
+				}
+				
+			}else {
+				//File
+				System.out.println("\tF:"+file.getName());
+			}
+		}
+		System.out.println("---------8.root directory 목록----------");
+		File[] rootDriverFileList=File.listRoots();
+		for (File file : rootDriverFileList) {
+			System.out.println(file.getPath());
+		}
+		System.out.println("c:/파일목록출력");
+		File cDrive=rootDriverFileList[0];
+		File[] cDriverFileList = cDrive.listFiles();
+		for (File file : cDriverFileList) {
+			System.out.println(file.getName());
+		}
+		System.out.println("---------------10.디렉토리생성----------------");
+		File newDir1 = new File("newDir1");
+		File newDir2 = new File("newDir2");
+		File newDir3 = new File("sample","subSample3");
+		File newDir4 = new File("C:/2022-11-JAVA-DEVELOPER/newDir4");
+		
+		System.out.println(">> newDir1 디렉토리 존재여부:"+newDir1.exists());
+		System.out.println(">> newDir2 디렉토리 존재여부:"+newDir2.exists());
+		System.out.println(">> newDir3 디렉토리 존재여부:"+newDir3.exists());
+		System.out.println(">> newDir4 디렉토리 존재여부:"+newDir4.exists());
+		
+		System.out.println(">> newDir1 디렉토리생성:"+newDir1.mkdir());
+		System.out.println(">> newDir2 디렉토리생성:"+newDir2.mkdir());
+		System.out.println(">> newDir3 디렉토리생성:"+newDir3.mkdir());
+		System.out.println(">> newDir4 디렉토리생성:"+newDir4.mkdir());
+		
+	
+				
 
 	}
 
